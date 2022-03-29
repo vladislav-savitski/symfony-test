ba8a5bb8f4a821ebefda753c457ab4280c554f56
Alban <alban.cbd@gmail.com>
2022-03-29 15:34:20 +0200

    add tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .enablePostCssLoader()
 ;
-
-module.exports = Encore.getWebpackConfig();
+    module.exports = Encore.getWebpackConfig();
